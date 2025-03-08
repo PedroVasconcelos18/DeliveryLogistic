cc7a29e7a424d3fb7949adb3d324ac8d1a492137
Pedro <pedrovasconcelos.dev@outlook.com>
2025-03-08 19:25:11 -0300

    first commit

---
+++ src/main/java/br/com/pedrohvss/deliverylogistic/DeliveryLogisticApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrohvss.deliverylogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryLogisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryLogisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pedrohvss/deliverylogistic/DeliveryLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedrohvss.deliverylogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryLogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
